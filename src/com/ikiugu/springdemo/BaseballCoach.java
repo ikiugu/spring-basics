611fd77841aafd9a249397cb77a6b7279b79f5f2
Alfred Ikiugu <ikiugualf@gmail.com>
2020-01-22 23:22:47 +0300

    simple oop class

---
+++ src/com/ikiugu/springdemo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.ikiugu.springdemo;
+
+public class BaseballCoach implements Coach{
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice.";
+	}
+}
