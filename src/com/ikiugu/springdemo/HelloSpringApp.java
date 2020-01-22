54e75211a7ec18c88decba27506d0c4363261bc0
Alfred Ikiugu <ikiugualf@gmail.com>
2020-01-22 23:23:43 +0300

    first working spring app
    the xml file creates the appcontext and the main method gets the bean from it

---
+++ src/com/ikiugu/springdemo/HelloSpringApp.java
@@ -0,0 +1,24 @@
+package com.ikiugu.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+
+		//load the spring config file
+		ClassPathXmlApplicationContext context = new 
+				ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve a bean from the container
+		Coach coach = context.getBean("myCoach", Coach.class);
+		
+		//call a method on the bean 
+		System.out.println(coach.getDailyWorkout());
+		
+		//close the app context
+		context.close();
+
+	}
+
+}
