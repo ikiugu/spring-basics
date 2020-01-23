e52aab0f7cca2853d39f2d1550c3998bcf197d9f
Alfred Ikiugu <ikiugualf@gmail.com>
2020-01-23 20:17:57 +0300

    added methods to test if the literal injection works

--- src/com/ikiugu/springdemo/SetterDemoApp.java
+++ src/com/ikiugu/springdemo/SetterDemoApp.java
@@ -14,6 +14,10 @@ public class SetterDemoApp {
 		
 		// call methods on the bean
 		 System.out.println(theCoach.getDailyFortune());
+		 
+		 // test for literal injection
+		 System.out.println(theCoach.getTeam());
+		 System.out.println(theCoach.getEmailAddress());
 		
 		// close context
 		appContext.close();
